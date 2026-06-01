478ea0c87aef4b1185f9e5e32027aad4747ba062
IsotoxalDev <isotoxal@proton.me>
2026-06-01 23:24:41 +0530

    Nvidia Drivers

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,41 @@
   # Locale
   i18n.defaultLocale = "en_US.UTF-8";
 
+  # NVIDIA
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:2:0:0";
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # for Wayland + NVIDIA
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fhdev=1"
+  ];
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    _GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  
   # User
   users.users.abhi = {
     isNormalUser = true;
